6048818b17ac3f16a76052c396393a800b533909
bipenk <bikkysar@gmail.com>
2023-10-16 22:05:07 -0400

    Second Commit

--- ecommerce-login-page.js
+++ ecommerce-login-page.js
@@ -1,3 +1,23 @@
+$(document).ready(function() {
+    $("#login-Form").submit(function(event) {
+        // Prevent the form from submitting normally
+        event.preventDefault();
+
+        // Check if the user ID and password fields are not empty
+        var userID = $("#username").val();
+        var password = $("#password").val();
+
+        if (userID.trim() === "" || password.trim() === "") {
+            alert("Please enter both User ID and Password.");
+        } else {
+            // Submit the form if both fields have values
+            alert("Logging in...");
+            // You can add an AJAX request here to submit the form data to the server.
+            // For example, you can use the $.ajax() function to send the data to a server-side script.
+        }
+    });
+});
+
 function validateLogin() {
     // Simulated user data for demonstration purposes
     const validUsername = "username";
